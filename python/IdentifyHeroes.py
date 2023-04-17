d47cb91d85d62367f76d0fb60e2cc8fb0fa6a8e3
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2023-04-17 13:32:49 +0900

    待機アイコンでの判定処理を追加

--- python/IdentifyHeroes.py
+++ python/IdentifyHeroes.py
@@ -4,17 +4,24 @@ import cv2
 def IdentifyHeroes(heroNames):
     maxSimilarity = 0
     identifiedHeroIndex = [None for i in range(5)]
-    for i in range(5):
-        for j, heroName in enumerate(heroNames):
-            templateImage = cv2.imread(
-                "templateImages/" + heroName + ".jpg")
-            originalImage = cv2.imread(
-                "originalImages/original" + str(i + 1) + ".jpg")
-            similarity = cv2.matchTemplate(
-                originalImage, templateImage, cv2.TM_CCOEFF_NORMED)[0][0]
-            if similarity > 0.9 and similarity > maxSimilarity:
-                maxSimilarity = similarity
-                identifiedHeroIndex[i] = j
-        # print(str(i + 1) + ":" + str(maxSimilarity)) 後で消す
-        maxSimilarity = 0
+    for i in range(2):
+        if None not in identifiedHeroIndex:
+            break
+        for i in range(5):
+            for j, heroName in enumerate(heroNames):
+                if i == 0 or i == 1 and identifiedHeroIndex[i] is None:
+                    templateImage = cv2.imread(
+                        "templateImages/" + heroName + ".jpg")
+                    originalImage = cv2.imread(
+                        "originalImages/original" + str(i + 1) + ".jpg")
+                    if i == 1:
+                        cv2.circle(templateImage, (width//2, height//2), 25, (0, 0, 0), -1)
+                        cv2.circle(templateImage, (width//2, height//2), 25, (0, 0, 0), -1)
+                    similarity = cv2.matchTemplate(
+                        originalImage, templateImage, cv2.TM_CCOEFF_NORMED)[0][0]
+                    if similarity > 0.9 and similarity > maxSimilarity:
+                        maxSimilarity = similarity
+                        identifiedHeroIndex[i] = j
+            maxSimilarity = 0
+            
     return identifiedHeroIndex
