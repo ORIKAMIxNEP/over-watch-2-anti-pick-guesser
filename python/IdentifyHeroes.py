95dc1c0c460c93c58ab5dfcbdb717e53d7bc9b93
ORIKAMI-NEP <ta.orikami@gmail.com>
2023-04-19 21:10:02 +0900

    待機アイコンの判定を追加

--- python/IdentifyHeroes.py
+++ python/IdentifyHeroes.py
@@ -7,21 +7,26 @@ def IdentifyHeroes(heroNames):
     for i in range(2):
         if None not in identifiedHeroIndex:
             break
-        for i in range(5):
-            for j, heroName in enumerate(heroNames):
-                if i == 0 or i == 1 and identifiedHeroIndex[i] is None:
-                    templateImage = cv2.imread(
-                        "templateImages/" + heroName + ".jpg")
-                    originalImage = cv2.imread(
-                        "originalImages/original" + str(i + 1) + ".jpg")
-                    if i == 1:
-                        cv2.circle(templateImage, (width//2, height//2), 25, (0, 0, 0), -1)
-                        cv2.circle(templateImage, (width//2, height//2), 25, (0, 0, 0), -1)
-                    similarity = cv2.matchTemplate(
-                        originalImage, templateImage, cv2.TM_CCOEFF_NORMED)[0][0]
-                    if similarity > 0.9 and similarity > maxSimilarity:
-                        maxSimilarity = similarity
-                        identifiedHeroIndex[i] = j
+        for j in range(5):
+            for k, heroName in enumerate(heroNames):
+                if i == 1 and k == 0 and identifiedHeroIndex[j] is not None:
+                    break
+                templateImage = cv2.imread(
+                    "templateImages/" + heroName + ".jpg")
+                originalImage = cv2.imread(
+                    "originalImages/original" + str(j + 1) + ".jpg")
+
+                if i == 1:
+                    width, height = templateImage.shape[:2]
+                    cv2.circle(templateImage, (width//2,
+                               height//2), 25, (0, 0, 0), -1)
+                    cv2.circle(originalImage, (width//2,
+                               height//2), 25, (0, 0, 0), -1)
+
+                similarity = cv2.matchTemplate(
+                    originalImage, templateImage, cv2.TM_CCOEFF_NORMED)[0][0]
+                if similarity > 0.8 and similarity > maxSimilarity:
+                    maxSimilarity = similarity
+                    identifiedHeroIndex[j] = k
             maxSimilarity = 0
-            
     return identifiedHeroIndex
